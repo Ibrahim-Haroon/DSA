7275442090d4bb21aab1aab4d648d5bd3db411c7
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-14 19:40:00 -0400

    example for using dynamic array.

--- main.c
+++ main.c
@@ -5,10 +5,21 @@
 //  Created by Ibrahim Haroon on 3/14/23.
 //
 
+#include "dynamic_array.h"
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    LIST test = list_init();
+    list_remove(test, 10);
+    for (int i = 0; i < 10; i++) {
+        list_add(test, i);
+    }
+    list_remove(test, 4);
+    list_remove(test, 0);
+    list_remove(test, 9);
+    print_list(test);
+    list_contains(test, 3) ? printf("Found!\n") : printf("Not in List\n");
+    list_destroy(&test);
+
     return 0;
 }
