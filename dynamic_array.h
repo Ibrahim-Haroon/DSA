cf6a87b0005b106f15d2b5a6117c0bbefa9a8f17
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-14 19:11:10 -0400

    header file.

--- dynamic_array.h
+++ dynamic_array.h
@@ -8,6 +8,18 @@
 #ifndef dynamic_array_h
 #define dynamic_array_h
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef void* LIST;
+
+//public methods
+LIST list_init(void);
+bool isEmpty(LIST list);
+void list_add(LIST list, int num);
+void list_remove(LIST list, int num);
+bool list_contains(LIST list, int num);
+void list_num_at(LIST list, int index);
+void print_list(LIST list);
+void list_destroy(LIST* list);
 
 #endif /* dynamic_array_h */
