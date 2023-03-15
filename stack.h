fd4c40678bb9411ebe338d5c2e1436c168cccc44
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-15 15:03:22 -0400

    header file.

--- stack.h
+++ stack.h
@@ -8,6 +8,16 @@
 #ifndef stack_h
 #define stack_h
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef void* STACK;
+//public
+STACK stack_init(void);
+bool stack_isEmpty(STACK stack);
+void stack_push(STACK stack, int num);
+void stack_pop(STACK stack);
+int stack_peak(STACK stack);
+void stack_print(STACK stack);
+void stack_destroy(STACK* stack);
 
 #endif /* stack_h */
