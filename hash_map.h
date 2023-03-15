f04dae681f09dec098cd20ad43cd7306c071d121
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-15 19:14:36 -0400

    header file.

--- hash_map.h
+++ hash_map.h
@@ -8,6 +8,16 @@
 #ifndef hash_map_h
 #define hash_map_h
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef void* HashMap;
+//public
+HashMap hashMap_init(void);
+bool hashMap_isEmpty(HashMap hashmap);
+void hashMap_insert(HashMap hashmap, int key, int value);
+void hashMap_remove_entry(HashMap hashmap, int key);
+bool hashMap_contains(HashMap hashmap, int key);
+void hashMap_print(HashMap hashmap);
+void hashMap_destroy(HashMap* hashmap);
 
 #endif /* hash_map_h */
