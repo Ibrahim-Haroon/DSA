464b3a8b6c60ea2f5c03b32e289e7cf0f27dd912
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-14 23:29:57 -0400

    example for using doubly linked list.

--- main.c
+++ main.c
@@ -5,19 +5,18 @@
 //  Created by Ibrahim Haroon on 3/14/23.
 //
 
-#include "singly_linked_list.h"
+#include "doubly_linked_list.h"
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    
-    SINGLY_LINKED_LIST test = ll_init();
+    DOUBLY_LINKED_LIST test = dll_init();
     for (int i = 0; i < 15; i++) {
-        ll_add(&test, i);
+        dll_add(&test, i);
     }
-    ll_remove(&test);
-    ll_remove_num(&test, 8); //need address of linked list in case the number being removed is the first node
-    ll_print(test);
-    ll_contains(test, 32) ? printf("YES\n") : printf("NO\n");
-    ll_destroy(&test);
+    dll_remove_num(&test, 9);
+    dll_remove(&test);
+    dll_print(test);
+    dll_contains(test, 23) ? printf("YES\n") : printf("NO\n");
+    dll_destroy(&test);
     return 0;
 }
