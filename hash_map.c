ab4cf4ee2660c55b93755de6824d765e1d9f4137
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-15 19:58:48 -0400

    minor tweaks.

--- hash_map.c
+++ hash_map.c
@@ -15,9 +15,9 @@ typedef struct node_m { //_m for map node
     struct node_m* next;
 }Node_m;
 unsigned int hash(unsigned int key, HashMap hashmap); //private
-bool hashMap_isFull(HashMap hashmap);    //private
-void increase_table_size(HashMap hashmap); //private
-Node_m* create_node_m(int key, int value);         //private
+bool hashMap_isFull(HashMap hashmap);                //private
+void increase_table_size(HashMap hashmap);          //private
+Node_m* create_node_m(int key, int value);          //private
 
 typedef struct map{
     int size;
