813fcac0adde21e6de981dc9d60bc67e08d05220
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-15 16:40:23 -0400

    header file.

--- queue.h
+++ queue.h
@@ -8,6 +8,15 @@
 #ifndef queue_h
 #define queue_h
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef void* QUEUE;
+
+QUEUE queue_init(void);
+bool queue_isEmpty(QUEUE queue);
+void enqueue(QUEUE queue, int num);
+void dequeue(QUEUE queue);
+void queue_print(QUEUE queue);
+void queue_destroy(QUEUE* queue);
 
 #endif /* queue_h */
