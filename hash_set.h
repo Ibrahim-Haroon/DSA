a2b788f0309c9ed7c24d7c019929ccf97d637591
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-15 19:57:52 -0400

    header file.

--- hash_set.h
+++ hash_set.h
@@ -8,6 +8,16 @@
 #ifndef hash_set_h
 #define hash_set_h
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef void* HashSet;
+//public
+HashSet hashSet_init(void);
+bool hashSet_isEmpty(HashSet hashset);
+void hashSet_add(HashSet hashset, int key);
+void hashSet_remove_entry(HashSet hashset, int key);
+bool hashSet_contains(HashSet hashset, int key);
+void hashSet_print(HashSet hashset);
+void hashSet_destroy(HashSet* hashset);
 
 #endif /* hash_set_h */
