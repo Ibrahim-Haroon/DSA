52418a03991929a162acc85a5a439c80529e19f8
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-15 16:40:39 -0400

    example of using queue.

--- main.c
+++ main.c
@@ -5,18 +5,18 @@
 //  Created by Ibrahim Haroon on 3/14/23.
 //
 
-#include "stack.h"
 #include <stdio.h>
+#include "queue.h"
 
 int main(int argc, char** argv) {
-    STACK test = stack_init();
-    for (int i = 0; i < 15; i++) {
-        stack_push(test, i);
+    QUEUE test = queue_init();
+    for (int i = 0; i < 5; i++) {
+        enqueue(test, i);
     }
-    stack_pop(test);
-    int peak = stack_peak(test);
-    printf("%d\n", peak);
-    stack_print(test);
-    stack_destroy(&test);
+    for (int i = 0; i < 3; i++) {
+        dequeue(test);
+    }
+    queue_print(test);
+    queue_destroy(&test);
     return 0;
 }
