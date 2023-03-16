4a29e26b03d4c499f3946302a1bea17ba28bb5ab
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-15 20:59:27 -0400

    header file.

--- stringBuilder.h
+++ stringBuilder.h
@@ -8,6 +8,15 @@
 #ifndef stringBuilder_h
 #define stringBuilder_h
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef void* StringBuilder;
+
+StringBuilder stringBuilder_init(void);
+bool string_isEmpty(StringBuilder string);
+void stringBuilder_append(char* anything, StringBuilder string);
+void stringBuilder_pop(StringBuilder string);
+void print_s(StringBuilder string);
+void stringBuilder_destroy(StringBuilder* string);
 
 #endif /* stringBuilder_h */
