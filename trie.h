bac57af81399acda02de5de53c358d3377f1b745
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-15 21:47:17 -0400

    header file.

--- trie.h
+++ trie.h
@@ -8,6 +8,15 @@
 #ifndef trie_h
 #define trie_h
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef void* TRIE;
+
+TRIE trie_init(void);
+bool trie_isEmpty(TRIE trie);
+void trie_insert(TRIE trie,char* word);
+bool trie_search(TRIE trie,char* word);
+void trie_printTrie(TRIE trie);
+void trie_destroyTrie(TRIE* trie);
 
 #endif /* trie_h */
