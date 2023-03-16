a468ff662757243d3a0d035135be7a63cc3c8691
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-15 21:49:16 -0400

    example of using a trie.

--- main.c
+++ main.c
@@ -6,9 +6,19 @@
 //
 
 #include <stdio.h>
-
+#include "trie.h"
 
 int main(int argc, char** argv) {
-
+    TRIE newTrie = trie_init();
+    trie_insert(newTrie, "cage");
+    trie_insert(newTrie, "abs");
+    trie_insert(newTrie, "bass");
+    trie_insert(newTrie, "cage");
+    trie_insert(newTrie, "cattle");
+    trie_insert(newTrie, "cage");
+    trie_printTrie(newTrie);
+    
+    trie_search(newTrie, "abs") ? printf("Found!\n") : printf("Not in Dictinary!\n");
+    trie_destroyTrie(&newTrie);
     return 0;
 }
