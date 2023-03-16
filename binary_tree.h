ed25ff3983373b22f73a6214540ee4b069b9c05a
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-16 11:18:24 -0400

    header file.

--- binary_tree.h
+++ binary_tree.h
@@ -8,6 +8,16 @@
 #ifndef binary_tree_h
 #define binary_tree_h
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef void* BST;
+//public
+BST bst_init(void);
+bool bst_isEmpty(BST tree);
+void bst_insert(BST tree, int value);
+void bst_remove(BST tree, int value);
+bool bst_contains(BST tree, int value);
+void bst_print(BST tree);
+void bst_destroy(BST* tree);
 
 #endif /* binary_tree_h */
