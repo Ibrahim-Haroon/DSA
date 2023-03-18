418287742c1e132bb4364f1fd2a7ff334379fc0d
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-18 19:23:28 -0400

    header file.

--- max_heap.h
+++ max_heap.h
@@ -8,6 +8,23 @@
 #ifndef max_heap_h
 #define max_heap_h
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef void* HEAP_MAX;
+
+HEAP_MAX maxHeap_init(void);
+bool maxHeap_isEmpty(HEAP_MAX heap);
+void maxHeap_insert(HEAP_MAX heap, int value);
+bool maxHeap_contain(HEAP_MAX heap, int value);
+void maxHeap_replace(HEAP_MAX heap, int value);
+int maxHeap_size(HEAP_MAX heap);
+int maxHeap_getMax(HEAP_MAX heap);
+int maxHeap_extractMax(HEAP_MAX heap);
+void maxHeap_heapify(HEAP_MAX heap);
+void maxHeap_remove(HEAP_MAX heap, int value);
+void maxHeap_removeMax(HEAP_MAX heap);
+void maxHeap_print(HEAP_MAX heap);
+void maxHeap_destroy(HEAP_MAX* heap);
+
 
 #endif /* max_heap_h */
