a0e955ca1c491bd0f8c0c25f8b4b3880fbc99989
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-18 21:34:05 -0400

    header file.:

--- min_heap.h
+++ min_heap.h
@@ -8,6 +8,21 @@
 #ifndef min_heap_h
 #define min_heap_h
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef void* HEAP_MIN;
+
+HEAP_MIN minHeap_init(void);
+void minHeap_insert(HEAP_MIN heap, int value);
+bool minHeap_contain(HEAP_MIN heap, int value);
+void minHeap_replace(HEAP_MIN heap, int value);
+bool minHeap_isEmpty(HEAP_MIN heap);
+int minHeap_size(HEAP_MIN heap);
+int minHeap_getMin(HEAP_MIN heap);
+int minHeap_extractMin(HEAP_MIN heap);
+void minHeap_remove(HEAP_MIN heap, int value);
+void minHeap_removeMin(HEAP_MIN heap);
+void minHeap_print(HEAP_MIN heap);
+void minHeap_destroy(HEAP_MIN* heap);
 
 #endif /* min_heap_h */
