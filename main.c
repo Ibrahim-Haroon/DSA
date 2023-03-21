4d7fbac53f9e6b7549e7b7cefbed51cda5ee33a0
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-03-21 16:51:21 -0400

    example of using graph.

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char** argv) {
     GRAPH test = graph_init();
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i < 9; i++) {
         graph_addVertex(test, i, rand() % 20);
     }
     graph_addConnection(test, 1, 2);
