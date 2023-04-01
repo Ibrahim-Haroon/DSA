dbc5a39eaffdbb74334190c3f7f04e123c5cd802
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-04-01 15:47:10 -0400

    added count based sorting.

--- sorting.h
+++ sorting.h
@@ -8,7 +8,7 @@
 #ifndef sorting_h
 #define sorting_h
 
-#define ARRAY_SIZE (5)
+#define ARRAY_SIZE (10)
 
 typedef enum sorting_technique {
     BUBBLE,     //O(n^2)
@@ -22,26 +22,31 @@ typedef enum sorting_technique {
     RADIX       //O(n) ***in some cases
 }Sort_By;
 
+typedef enum time{
+    withTime,
+    withoutTime
+}TIME;
+
 int* create_unsorted_arr(void);
-void sort(int* arr, Sort_By technique);
+void sort(int* arr, Sort_By technique, TIME timed);
 //definition: sorting algorithm that compares two adjacent elements and swaps them if they are out of order, it does this for the entire array for size - 1 times. Because at the end, the last item can only be in the correct spot since all other spots are taken.
-void bubble_sort(int* arr);
+double bubble_sort(int* arr);
 //definition: sorting algorithm that finds the smallest item in the unsorted portion of the array and moves it to the sorted portion of the array, it does this size - 1 times. Because at the end, the last item can only be in the correct spot since all other spots are taken.
-void selection_sort(int* arr);
-//definition: sorting algorithm that finds the item of place in the unsorted portion of the array, and keeps moving the item that is the incorrect place to the left, which is the sorted portion. It does this size - 1 times. Because at the end, the last item can only be in the correct spot since all other spots are taken.
-void insertion_sort(int* arr);
-//definition: sorting algorithm that compares items H away from each other, and if they are out of place, it swaps them. This is done size - 1 times. Because at the end, the last item can only be in the correct spot since all other spots are taken.
-void shell_sort(int* arr);
+double selection_sort(int* arr);
+//definition: sorting algorithm that finds the item of place in the unsorted portion of the array, and keeps moving the item that is in the incorrect place to the left, which is the sorted portion. It does this size - 1 times. Because at the end, the last item can only be in the correct spot since all other spots are taken.
+double insertion_sort(int* arr);
+//definition: sorting algorithm that compares and swaps items that are at certain gap, h,  distance apart. The algorithm starts with a larger gap size and reduces it over multiple passes until the gap size becomes 1, at which point the algorithm becomes equivalent to an insertion sort.
+double shell_sort(int* arr);
 //definition:
-void heap_sort(int* arr);
+double heap_sort(int* arr);
 //definition:
-void quick_sort(int* arr);
+double quick_sort(int* arr);
 //definition:
-void merge_sort(int* arr);
+double merge_sort(int* arr);
 //definition:
-void count_sort(int* arr);
+double count_sort(int* arr);
 //definition:
-void radix_sort(int* arr);
+double radix_sort(int* arr);
 void sort_print(int *arr);
 void sort_destroy(int* arr);
 
