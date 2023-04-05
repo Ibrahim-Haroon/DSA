45328cdde29ae92deb8a7ec9a2b0265aabe4afcc
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-04-05 15:19:12 -0400

    example of using some graph methods.

--- main.c
+++ main.c
@@ -4,13 +4,22 @@
 //
 //  Created by Ibrahim Haroon on 3/14/23.
 //
-#include "max_heap.h"
+#include "graph.h"
+#include <stdio.h>
 
-int main() {
-    int arr[] = {17, 45, 36, 78, 92, 101, 32, 17, 89, 102};
-    HEAP_MAX test = maxHeap_init();
-    maxHeap_heapify(test, arr, sizeof(arr)/sizeof(arr[0]));
-    maxHeap_print(test);
-    maxHeap_destroy(&test);
+int main(void) {
+    GRAPH g = graph_init();
+    for (int i = 1; i <= 8; i++)
+        graph_addVertex(g, i);
+    
+    graph_addConnection(g, 1, 4, 5);
+    graph_addConnection(g, 1, 6, 6);
+    LIST res = graph_getAdjacentVerticies(g, 1);
+    list_print(res);
+    int weight = graph_getWeight(g, 1, 6);
+    printf("%d\n", weight);
+    graph_destroy(&g);
     return 0;
 }
+
+
